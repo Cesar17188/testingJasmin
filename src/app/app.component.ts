ddab152043733aad7b93271b424ce87eb0062e89
cesareli17188 <cesareli17188@gmail.com>
2022-05-06 18:05:26 -0500

    primeras pruebas, explorando marchers

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Calculator } from './calculator';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'testingJasmin';
+
+  ngOnInit(): void {
+    const calculator = new Calculator();
+    const rta = calculator.multiply(3,3);
+    console.log(rta === 9);
+    const rta2 = calculator.divide(3, 0);
+    console.log(rta2 === null);
+    console.log(rta2);
+  }
 }
