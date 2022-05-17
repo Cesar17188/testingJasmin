68c8485381ff84b9690fc2ab9b367fd9ef20df21
cesareli17188 <cesareli17188@gmail.com>
2022-05-17 18:59:39 -0500

    generando mocks

--- src/app/services/product.service.spec.ts
+++ src/app/services/product.service.spec.ts
@@ -3,6 +3,7 @@ import { HttpClientTestingModule, HttpTestingController } from '@angular/common/
 
 import { ProductsService } from './product.service';
 import { Product } from '../models/product.model';
+import { generateManyProducts, generateOneProduct } from '../models/product.mock';
 import { environment } from '../../environments/environment';
 
 fdescribe('ProductsService', () => {
@@ -27,33 +28,70 @@ fdescribe('ProductsService', () => {
   });
 
   describe('tests for getAllSimple', () => {
-    it('should return a product list', () => {
+    it('should return a product list', (doneFn) => {
       //Arrange
-      const mockData: Product[] = [
-        {
-          id: '123',
-          title: 'title',
-          price: 12,
-          description: 'blablabla',
-          category: {
-            id: 112,
-            name: 'as'
-          },
-          images: ['img', 'img']
-        }
-      ];
+      const mockData: Product[] = generateManyProducts(2);
       //Act
       productsService.getAllSimple()
       .subscribe((data) => {
         //Assert
         expect(data.length).toEqual(mockData.length);
-        //doneFn();
+        expect(data).toEqual(mockData);
+        doneFn();
       });
       //http config
       const url = `${environment.API_URL}/api/v1/products`;
       const req = httpController.expectOne(url);
       req.flush(mockData);
       httpController.verify();
+    });
+  });
+
+  describe('tests for getAll', () => {
+    it('should return a product list', (doneFn) => {
+      //Arrange
+      const mockData: Product[] = generateManyProducts(3);
+      //Act
+      productsService.getAll()
+      .subscribe((data) => {
+        //Assert
+        expect(data.length).toEqual(mockData.length);
+
+        doneFn();
+      });
+      //http config
+      const url = `${environment.API_URL}/api/v1/products`;
+      const req = httpController.expectOne(url);
+      req.flush(mockData);
+      httpController.verify();
+    });
+
+    it('should return product list width taxes', (doneFn) => {
+      // Arrange
+      const mockData: Product[] = [
+        {
+          ...generateOneProduct(),
+          price: 100, //100 * .19 = 19   
+        },
+        {
+          ...generateOneProduct(),
+          price: 200, //200 * .19 = 38   
+        },
+      ];
+      //Act
+      productsService.getAll()
+      .subscribe((data) => {
+        //Assert
+        expect(data.length).toEqual(mockData.length);
+        expect(data[0].taxes).toEqual(19);
+        expect(data[1].taxes).toEqual(38);
+        doneFn();
+      });
+       //http config
+       const url = `${environment.API_URL}/api/v1/products`;
+       const req = httpController.expectOne(url);
+       req.flush(mockData);
+       httpController.verify();
     });
   });
 });
