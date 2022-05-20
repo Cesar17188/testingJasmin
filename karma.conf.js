c88efe899d5fee7be7096ef0f7801a15295fd376
cesareli17188 <cesareli17188@gmail.com>
2022-05-20 18:34:37 -0500

    launcher fixed

--- karma.conf.js
+++ karma.conf.js
@@ -47,7 +47,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['ChromeHeadless'],
-    custonLauchers: {
+    custonLaunchers: {
       ChromeHeadlessCI: {
         base: 'ChromeHeadless',
         flags: ['--no-sandbox']
