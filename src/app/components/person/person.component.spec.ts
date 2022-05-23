a8b04147b451b6fd65f569134ff901161964c3e9
cesareli17188 <cesareli17188@gmail.com>
2022-05-23 18:26:36 -0500

    debugElement y byCss

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,4 +1,6 @@
+import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 
 import { PersonComponent } from './person.component';
 
@@ -24,8 +26,17 @@ fdescribe('PersonComponent', () => {
   });
 
   it('should have <p> with "Soy un párrafo"</p>', () => {
-    const personElement: HTMLElement = fixture.nativeElement;
-    const p = personElement.querySelector('p');
-    expect(p?.textContent).toEqual('Soy un párrafo');
+    const personDebug: DebugElement = fixture.debugElement;
+    const pDebug: DebugElement = personDebug.query(By.css('p'));
+    const pElement: HTMLElement = pDebug.nativeElement;
+    expect(pElement?.textContent).toEqual('Soy un párrafo');
   });
+
+  it('should have <h3> with "Hola, PersonComponent"</h3>', () => {
+    const personDebug: DebugElement = fixture.debugElement;
+    const h3Debug: DebugElement = personDebug.query(By.css('h3'));
+    const h3: HTMLElement = h3Debug.nativeElement;
+    expect(h3?.textContent).toEqual('Hola, PersonComponent');
+  });
+
 });
