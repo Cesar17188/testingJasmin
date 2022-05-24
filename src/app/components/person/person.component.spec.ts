63f47c4a4836c65095620541b104c0c880d78756
cesareli17188 <cesareli17188@gmail.com>
2022-05-23 19:36:33 -0500

    componentes con inputs

--- src/app/components/person/person.component.spec.ts
+++ src/app/components/person/person.component.spec.ts
@@ -1,6 +1,7 @@
 import { DebugElement } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
+import { Person } from 'src/app/models/person.model';
 
 import { PersonComponent } from './person.component';
 
@@ -18,6 +19,7 @@ fdescribe('PersonComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PersonComponent);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
@@ -25,18 +27,35 @@ fdescribe('PersonComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have <p> with "Soy un párrafo"</p>', () => {
-    const personDebug: DebugElement = fixture.debugElement;
-    const pDebug: DebugElement = personDebug.query(By.css('p'));
-    const pElement: HTMLElement = pDebug.nativeElement;
-    expect(pElement?.textContent).toEqual('Soy un párrafo');
+  it('should the name be "Cesar"', () => {
+    component.person = new Person('Cesar', 'Armendariz', 29, 120, 1.75);
+    expect(component.person.name).toEqual('Cesar');
   });
 
-  it('should have <h3> with "Hola, PersonComponent"</h3>', () => {
+  it('should have <p> with "Mi altura es {person.heigth}"</p>', () => {
+    // Arrange
+    component.person = new Person('Valentina', 'Armendariz', 29, 120, 1.75);
+    const personDebug: DebugElement = fixture.debugElement;
+    const expectedMsg = `Mi altura es ${component.person.heigth} cm`;
+    const pDebug: DebugElement = personDebug.query(By.css('p'));
+    const pElement: HTMLElement = pDebug.nativeElement;
+    // Act
+    fixture.detectChanges();
+    // Assert
+    expect(pElement?.textContent).toContain(expectedMsg);
+  });
+
+  it('should have <h3> with "Hola, {person.name}"</h3>', () => {
+    // Arrange
+    component.person = new Person('Valentina', 'Armendariz', 29, 120, 1.75);
+    const expectedMsg = `Hola, ${component.person.name}`;
     const personDebug: DebugElement = fixture.debugElement;
     const h3Debug: DebugElement = personDebug.query(By.css('h3'));
     const h3: HTMLElement = h3Debug.nativeElement;
-    expect(h3?.textContent).toEqual('Hola, PersonComponent');
+    // Act
+    fixture.detectChanges();
+    //Assert
+    expect(h3?.textContent).toContain(expectedMsg);
   });
 
 });
