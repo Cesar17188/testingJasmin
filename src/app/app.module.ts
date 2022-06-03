df869d92b4fdec9a2f1f9eddbf6d9f3b41c99e6e
cesareli17188 <cesareli17188@gmail.com>
2022-06-03 18:47:23 -0500

    creando pipes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { PeopleComponent } from './components/people/people.component';
 import { ProductComponent } from './components/product/product.component';
 import { OthersComponent } from './components/others/others.component';
 import { HighligthDirective } from './directives/highligth.directive';
+import { ReversePipe } from './pipes/reverse.pipe';
 
 @NgModule({
   declarations: [
@@ -24,7 +25,8 @@ import { HighligthDirective } from './directives/highligth.directive';
     PeopleComponent,
     ProductComponent,
     OthersComponent,
-    HighligthDirective
+    HighligthDirective,
+    ReversePipe
   ],
   imports: [
     BrowserModule,
