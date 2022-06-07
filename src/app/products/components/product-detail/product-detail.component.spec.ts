7387cbe054444a00f0343a13c13666a32bec940c
cesareli17188 <cesareli17188@gmail.com>
2022-06-07 15:04:20 -0500

    pruebas al render y pruebas invalidas

--- src/app/products/components/product-detail/product-detail.component.spec.ts
+++ src/app/products/components/product-detail/product-detail.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
-import { ActivatedRouteStub, mockObservable } from './../../../../testing';
+import { ActivatedRouteStub, getText, mockObservable } from './../../../../testing';
 import { ProductsService } from '../../../services/product.service';
 import { generateOneProduct } from '../../../models/product.mock';
 
@@ -36,21 +36,52 @@ fdescribe('ProductDetailComponent', () => {
     route = TestBed.inject(ActivatedRoute) as unknown as ActivatedRouteStub;
     productsService = TestBed.inject(ProductsService) as unknown as jasmine.SpyObj<ProductsService>;
     location = TestBed.inject(Location) as unknown as jasmine.SpyObj<Location>;
+  });
+
+  it('should create', () => {
 
     const productId = '1';
     route.setParamMap({id: productId});
 
     const productMock = {
-      ...generateOneProduct(), 
+      ...generateOneProduct(),
       id: productId,
     }
 
     productsService.getOne.and.returnValue(mockObservable(productMock));
 
     fixture.detectChanges(); // ngOnInit
+    expect(component).toBeTruthy();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('should show the product in the view', () => {
+
+    const productId = '2';
+    route.setParamMap({id: productId});
+
+    const productMock = {
+      ...generateOneProduct(),
+      id: productId,
+    }
+
+    productsService.getOne.and.returnValue(mockObservable(productMock));
+
+    fixture.detectChanges(); // ngOnInit
+
+    const titleText = getText(fixture, 'title');
+    const priceText = getText(fixture, 'price');
+    expect(titleText).toContain(productMock.title);
+    expect(priceText).toContain(productMock.price);
+    expect(productsService.getOne).toHaveBeenCalledWith(productId);
+  });
+
+  it('should go to back without id params', () => {
+
+    route.setParamMap({  });
+
+    location.back.and.callThrough(); //mocking
+
+    fixture.detectChanges(); // ngOnInit
+    expect(location.back).toHaveBeenCalled();
   });
 });
